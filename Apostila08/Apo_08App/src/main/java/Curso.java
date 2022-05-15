e6dc63cb4007750d4fb5501a6e10dc583162751f
Raul Loula <bohrs@hotmail.com>
2022-05-15 19:15:05 -0300

    .

---
+++ Apostila08/Apo_08App/src/main/java/Curso.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bohrs
+ */
+public class Curso {
+    private int id;
+    private String nome;
+    private String tipo;
+    
+    public Curso(int id, String nome, String tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getTipo() {
+        return tipo;
+    }
+    
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    @Override
+    public String toString() 
+    {
+        return this.nome;
+    }
+
+}
